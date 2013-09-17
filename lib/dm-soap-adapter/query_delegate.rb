ad7c34996134ee418bd475e074e591221c19ba57
Nass <nasali@anypresence.com>
2013-09-17 14:10:59 -0400

    Refactored query builder.

--- lib/dm-soap-adapter/query_delegate.rb
+++ lib/dm-soap-adapter/query_delegate.rb
@@ -11,17 +11,11 @@ module DataMapper
                 query_hash[wsdl_remote_name] = value
               end
             end
-          end
-=begin
-          query_hash[:model] = query.model.storage_name(query.repository)          
-          query_hash[:fields] = build_queried_fields(query.fields)
-          query_hash[:conditions] = build_query_conditions(query.conditions) if query.conditions
-          query_hash[:order] = build_query_order(query.order) unless query.order.nil?
-          query_hash[:limit] = query.limit unless query.limit.nil?
-          query_hash[:offset] = query.offset unless query.offset.nil?
-=end          
+          end         
           query_hash
         end
+        
+        private
         
         def find_condition_value_for_property_name(conditions, property_name)
           conditions.each do |condition| 
@@ -32,25 +26,6 @@ module DataMapper
           return nil
         end
         
-        def build_queried_fields(properties)
-          properties.collect{|property| property.field.to_sym }
-        end
-        
-        def build_query_conditions(conditions)
-          conditions.collect do |condition|
-            if condition.instance_of? DataMapper::Query::Conditions::EqualToComparison
-              {:equal => [condition.subject.field.to_sym, condition.loaded_value]}
-            else
-              raise "Implement me for #{operand.class}"
-            end
-          end
-        end
-        
-        def build_query_order(order)
-          order.collect do |direction|
-            {direction.target.field.to_sym => direction.operator}
-          end
-        end
       end
     end
   end
